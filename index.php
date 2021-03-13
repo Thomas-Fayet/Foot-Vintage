5c72198158960b14ff3c4e9526a04f6cb4306c87
Thomas-Fayet <t.fayet@hotmail.fr>
2021-03-13 19:47:00 +0100

    commit homepage

--- index.php
+++ index.php
@@ -6,30 +6,30 @@
         <?php include 'config/template/nav.php'; ?>
     </header>
     <main>
-        <section class="home-wrapper">
+        <section class="main-wrapper">
             <div class="news-block">
-                <h2>VOIR LES NOUVEAUTÉS</h2>
+                <h2 class="title-boxes news-title">VOIR TOUT LES MAILLOTS</h2>
             </div>
             <section class="championship-wrapper">
                 <div class="championship-boxes championship-one">
-                    <h2>LIGUE 1</h2>
+                    <h2 class="title-boxes">LIGUE 1</h2>
                 </div>
                 <div class="championship-boxes championship-two">
-                    <h2>SERIE A</h2>
+                    <h2 class="title-boxes">SERIE A</h2>
                 </div>
                 <div class="championship-boxes championship-three">
-                    <h2>BPL</h2>
+                    <h2 class="title-boxes">BPL</h2>
                 </div>
                 <div class="championship-boxes championship-four">
-                    <h2>LIGA</h2>
+                    <h2 class="title-boxes">LIGA</h2>
                 </div>
             </section>
             <section class="country-player-wrapper">
                 <div class="country-player-block country-block">
-                    <h2>SELECTION</h2>
+                    <h2 class="title-boxes">SELECTION</h2>
                 </div>
                 <div class="country-player-block player-block">
-                    <h2>JOUEURS</h2>
+                    <h2 class="title-boxes">JOUEURS</h2>
                 </div>
             </section>
         </section>
