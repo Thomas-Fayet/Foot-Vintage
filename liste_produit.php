268fbd33a52a67d2013c4762e57271b1304c075d
RolleriLuca <lrolleri3053@gmail.com>
2021-03-14 00:34:09 +0100

    menuburger-header

---
+++ liste_produit.php
@@ -0,0 +1,22 @@
+<?php include 'config/template/head.php'; ?>
+<body>
+    <header>
+        <?php include 'config/template/nav.php'; ?>
+    </header>
+    <main>
+        <section class="main-wrapper">
+            <div class="shop-block">
+                <h2 class="title-boxes news-title">LA BOUTIQUE</h2>
+            </div>
+
+            <section class="products-wrapper">
+                <div class="product-card product-left">
+                    
+                </div>
+            </section>
+            
+        </section>
+    </main>
+    <?php include 'config/template/footer.php'; ?>
+</body>
+</html>
