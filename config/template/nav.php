d366a3ebc554195b031c545651db451bc61cac75
Thomas-Fayet <t.fayet@hotmail.fr>
2021-03-13 20:17:40 +0100

    commit nav

---
+++ config/template/nav.php
@@ -0,0 +1,13 @@
+<div class="header-top">
+    <h1 class="header-logo">foot vintage</h1>
+
+
+</div>
+
+
+<nav class="nav">
+    <a class="nav-link" href="#">Nouveautés</a>
+    <a class="nav-link" href="#">Selections</a>
+    <a class="nav-link" href="#">Clubs</a>
+    <a class="nav-link" href="#">Joueurs</a>
+</nav>
