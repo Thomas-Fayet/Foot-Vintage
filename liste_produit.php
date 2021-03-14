551f406a62458f160afcd714b27fd7791f4db1af
RolleriLuca <lrolleri3053@gmail.com>
2021-03-14 20:20:34 +0100

    creation liste_produit.php

--- liste_produit.php
+++ liste_produit.php
@@ -10,13 +10,45 @@
             </div>
 
             <section class="products-wrapper">
-                <div class="product-card product-left">
-                    
+                <div class="product-boxes product-one">
+                    <p class="products-description products-info"> AS Roma 2012/13 - Domicile</p>  
+                    <div class="separator-info"> </div> 
+                    <p class="products-price products-info"> 90 €</p> 
+                </div>
+
+                <div class="product-boxes product-two">
+                    <p class="products-description products-info"> Juventus 1998/99 - Domicile</p>  
+                    <div class="separator-info"> </div> 
+                    <p class="products-price products-info"> 85 €</p> 
+                </div>
+
+                <div class="product-boxes product-three">
+                    <p class="products-description products-info"> Inter Milan 2009/10 - Extérieur</p>  
+                    <div class="separator-info"> </div> 
+                    <p class="products-price products-info"> 75 €</p> 
+                </div>
+
+                <div class="product-boxes product-one">
+                    <p class="products-description products-info"> AS Roma 2012/13 - Domicile</p>  
+                    <div class="separator-info"> </div> 
+                    <p class="products-price products-info"> 90 €</p> 
+                </div>
+
+                <div class="product-boxes product-two">
+                    <p class="products-description products-info"> Juventus 1998/99 - Domicile</p>  
+                    <div class="separator-info"> </div> 
+                    <p class="products-price products-info"> 85 €</p> 
+                </div>
+
+                <div class="product-boxes product-three">
+                    <p class="products-description products-info"> Inter Milan 2009/10 - Extérieur</p>  
+                    <div class="separator-info"> </div> 
+                    <p class="products-price products-info"> 75 €</p> 
                 </div>
             </section>
             
         </section>
     </main>
-    <?php include 'config/template/footer.php'; ?>
+    <!-- <?php include 'config/template/footer.php'; ?> -->
 </body>
 </html>
